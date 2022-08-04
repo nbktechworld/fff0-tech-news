eeefae5f381b0267eb15b2f588c8ebf9f30f606f
nbkhope <ryumemaru@yahoo.com>
2022-08-04 01:04:39 -0400

    Implement shared header and main content style

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Link from 'next/link';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <header>
+        <Navbar bg="dark" variant="light">
+          <Container>
+            <Navbar.Brand href="/" as={Link}>
+              <a className="text-bg-dark">Tech News</a>
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+      </header>
+      <Container as="main" className="pt-1">
+        <Row className="justify-content-lg-center">
+          <Col lg={6}>
+            <Component {...pageProps} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- client/pages/articles/[articleSlug].js
+++ client/pages/articles/[articleSlug].js
@@ -1,10 +1,7 @@
 import { useRouter } from 'next/router';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Link from 'next/link';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import articles from '../../articles';
 
@@ -12,26 +9,22 @@ export default function ArticleSlug(props) {
   // const router = useRouter();
 
   return (
-    <Container>
-      <Row className="justify-content-lg-center">
-        <Col lg={6}>
-          <Breadcrumb>
-            <Breadcrumb.Item linkAs={Link} href="/articles">
-              <a>Articles</a>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active>
-              <a>Article</a>
-            </Breadcrumb.Item>
-          </Breadcrumb>
-          <Card>
-            <Card.Body>
-              <Card.Title>{props.article.title}</Card.Title>
-              {props.article.body}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Breadcrumb>
+        <Breadcrumb.Item linkAs={Link} href="/articles">
+          <a>Articles</a>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>
+          <a>Article</a>
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <Card>
+        <Card.Body>
+          <Card.Title>{props.article.title}</Card.Title>
+          {props.article.body}
+        </Card.Body>
+      </Card>
+    </>
   )
 }
 
