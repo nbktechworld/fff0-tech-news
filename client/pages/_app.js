41a32412fe5d96e078f130b8e85fc01adb395690
nbkhope <ryumemaru@yahoo.com>
2022-08-05 21:52:45 -0400

    Finish implementing the spinner centered on the screen and install sass

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.scss';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Link from 'next/link';
@@ -45,10 +46,13 @@ function App({ Component, pageProps }) {
       <Container as="main" className="pt-1">
         <Row className="justify-content-lg-center">
           <Col lg={6}>
-            <div>Loading: {loading ? 'true': 'false'}</div>
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading</span>
-            </Spinner>
+            {loading && (
+              <div className="spinner-container">
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading</span>
+                </Spinner>
+              </div>
+            )}
             <Component {...pageProps} />
           </Col>
         </Row>
