f3f9c69d410458f2be5d5849d59b853a0adaaa76
nbkhope <ryumemaru@yahoo.com>
2022-08-05 20:07:00 -0400

    Handle errors not in the 200s range in articles page

--- client/pages/articles/index.js
+++ client/pages/articles/index.js
@@ -1,10 +1,16 @@
 import { Card } from 'react-bootstrap';
 import Link from 'next/link';
+import Alert from 'react-bootstrap/Alert';
 
 export default function ArticlesIndex(props) {
   return (
     <>
       <div className="mb-2">Articles</div>
+      {props.articlesError && (
+        <Alert variant="danger">
+          {props.articlesError}
+        </Alert>
+      )}
       <div>
         {props.articles.map((article) => (
           <Card className="mb-2" key={article.id}>
@@ -22,11 +28,18 @@ export default function ArticlesIndex(props) {
 
 export async function getServerSideProps() {
   const response = await fetch('http://localhost:3001/articles');
-  const articles = await response.json();
+
+  const props = {
+    articles: []
+  };
+  if (response.ok) {
+    props.articles = await response.json();
+  }
+  else {
+    props.articlesError = `${response.status} ${response.statusText}`;
+  }
 
   return {
-    props: {
-      articles
-    }
+    props: props
   }
 }
