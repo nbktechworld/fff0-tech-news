dd6e865e46ef6cee8ea9ffc0f261beb031c70fc5
nbkhope <ryumemaru@yahoo.com>
2022-08-06 22:46:55 -0400

    Add migration to create table articles and model for Article

---
+++ server/migrations/20220807022544-create-article.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      slug: {
+        allowNull: false,
+        type: Sequelize.STRING(128)
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING(128)
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.STRING(4096)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('articles');
+  }
+};
---
+++ server/models/article.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Article.init({
+    slug: DataTypes.STRING,
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Article',
+  });
+  return Article;
+};
