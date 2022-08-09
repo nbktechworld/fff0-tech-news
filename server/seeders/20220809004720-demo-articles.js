7a7839476473d41c26b7d0536430e6be7a86797e
nbkhope <ryumemaru@yahoo.com>
2022-08-08 20:58:02 -0400

    Add seed file for demo articles

---
+++ server/seeders/20220809004720-demo-articles.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('articles', [
+      {
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+        slug: 'lorem-ipsum-dolor-sit-amet-consectetur-adipiscing',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        title: 'Ut enim ad minim veniam, quis nostrud exercitation',
+        slug: 'ut-enim-ad-minim-veniam',
+        body: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        title: 'Duis aute irure dolor in reprehenderit',
+        slug: 'duis-aute-irure-dolor-in-reprehenderit',
+        body: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('articles', null, {});
+  }
+};
