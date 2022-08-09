3d1bf371b9f00336d52d4baa1ba1ac2c6f0e498b
nbkhope <ryumemaru@yahoo.com>
2022-08-08 20:39:26 -0400

    Rename table SequelizeMeta to sequelize_meta

--- server/config/config.js
+++ server/config/config.js
@@ -7,20 +7,23 @@ module.exports = {
     "password": process.env.DATABASE_PASSWORD,
     "database": "tech_news_development",
     "host": "127.0.0.1",
-    "dialect": "postgres"
+    "dialect": "postgres",
+    migrationStorageTableName: "sequelize_meta"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    migrationStorageTableName: "sequelize_meta"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    migrationStorageTableName: "sequelize_meta"
   }
 }
