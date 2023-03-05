b6766c306d964de5d4e39ae84fc4c3ef0e2fada7
nbkhope <ryumemaru@yahoo.com>
2023-03-05 00:28:54 -0500

    Add next lint

--- client/components/MarkdownRegion.js
+++ client/components/MarkdownRegion.js
@@ -10,7 +10,7 @@ export default function MarkdownRegion(props) {
         console.log(imgProps);
         const TheComponent = Image;
         return (
-          <a href={imgProps.src} target="_blank">
+          <a href={imgProps.src} target="_blank" rel="noreferrer">
             <TheComponent {...imgProps.node.properties} width="1920" height="1080" style={{ maxWidth: '100%', height: 'auto' }} />
           </a>
         );
