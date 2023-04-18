41ab76577bc06d8f79ea5ffcd46b7c407d660784
nbkhope <ryumemaru@yahoo.com>
2023-04-18 00:08:28 -0400

    Remove AWS S3 image files when you reverse the migration to create the articles table (that way, thumbnail images are deleted and dont linger)

--- server/migrations/20220807022544-create-article.js
+++ server/migrations/20220807022544-create-article.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const ImageService = require('../services/image-service');
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('articles', {
@@ -39,6 +42,12 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
+    const imageService = new ImageService();
+    const articleData = await queryInterface.sequelize.query('SELECT thumbnail_url FROM articles WHERE thumbnail_url IS NOT NULL', { type: queryInterface.sequelize.QueryTypes.SELECT });
+    for (const { thumbnail_url } of articleData) {
+      await imageService.remove(imageService.getKeyForUrl(thumbnail_url));
+    }
+
     await queryInterface.dropTable('articles');
   }
 };
